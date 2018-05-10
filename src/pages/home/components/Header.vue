7562f6bb58dc960fa6f6ed80425f472e9c4bb428
papaya <1643700595@qq.com>
2018-05-10 18:19:58 +0800

    增加header.vue,使用stylus写样式，配置webpack，给文件路径添加别名，添加styl变量文件并引用

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+       城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+// @import '../../../assets/styles/variables.styl'
+  // @import '~@/assets/styles/variables.styl'
+  // 给styles目录起一个别名 
+  // 修改了webpack配置需要重启服务器
+  
+  @import '~styles/variables.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+    .back-icon
+      font-size: .4rem
+      text-align: center
+    .header-input
+      flex: 1
+      background: #fff
+      border-radius: .1rem
+      height: .62rem
+      line-height: .62rem
+      margin-top: .12rem
+      margin-left: .2rem
+      color: #ccc
+      padding-left: .2rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
+        margin-left: -.04rem
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    hello world
+    <home-header></home-header>
   </div>
 </template>
 <script>
+// 引入一个局部组件，使用的时候，必须在components中声明局部组件
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
