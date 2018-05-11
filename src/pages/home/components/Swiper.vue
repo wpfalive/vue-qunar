0aa3bbd0d6d58ce4b96d69874bdd633c3ed8ee5a
papaya <1643700595@qq.com>
2018-05-11 15:36:12 +0800

    swiper part finished

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    swiper
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_750x200_b99b8e07.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1804/1b/6c766b79e2045102.jpg_750x200_29ad1225.jpg" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 <script>
 export default {
-  name: 'HomeSwiper'
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.wrapper
+  overflow hidden
+  height 0
+  width 100%
+  padding-bottom 26.67%
+  .swiper-img
+    width 100%
 </style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,6 +2,7 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <div>text</div>
   </div>
 </template>
 <script>
